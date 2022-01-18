c7b100e2220c6624443fe1050d8ec13811581aef
saiful Islam <saifulmeraz897@gmail.com>
2022-01-18 19:13:16 +0600

    child to parent data emit

---
+++ src/components/CreateFriend.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <div class="row">
+          <div class="col-md-4 bg-success p-4">
+             <form @submit.prevent="formSubmit">
+                <label class="text-white">Name:</label>
+                <input type="name" class="form-control" v-model="name">
+                <label class="text-white">Phone Number:</label>
+                <input type="number" class="form-control" v-model="phone">
+                <label class="text-white">Email:</label>
+                <input type="email" class="form-control" v-model="email">
+                <button class="btn btn-info mt-4">Submit</button>
+            </form> 
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'CreateFriend',
+    data(){
+        return{
+                name:'',
+                phone:'',
+                email:''
+        }
+    },
+    methods:{
+        formSubmit(){
+            this.$emit('adduser',
+            this.name,
+            this.phone,
+            this.email )
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,14 +1,14 @@
 <template>
 <div>
     <h1>This is our FriendContact vue!!</h1>
-    <h3>{{ fullName }}</h3>
+    <h3>{{ fName }} {{ lName  }} {{email}}</h3>
 </div>
 </template>
 
 <script>
 export default {
     name:'FriendContact',
-    props:['fullName'],
+    props:['fName','lName','email'],
     data(){
         return{
             name:'saiful'
